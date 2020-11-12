4deb629f5d2da61e8d73b2378f3e4523e810628a
niuzhiweimr <niuzhiwei1314@126.com>
2020-11-12 11:19:20 +0800

    优化方法注释

--- askwalking-common/src/main/java/com/cloud/askwalking/common/tool/IDTool.java
+++ askwalking-common/src/main/java/com/cloud/askwalking/common/tool/IDTool.java
@@ -6,17 +6,8 @@ import java.util.UUID;
  * @author niuzhiwei
  */
 public class IDTool {
-    /**
-     * 功能描述: <br>
-     * 〈生成uuid〉
-     *
-     * @Param:
-     * @Return:
-     * @Author: luwan
-     * @Date: 2020/6/29 16:10
-     */
-    public static String generatePK() {
 
+    public static String generatePK() {
         return UUID.randomUUID().toString().replaceAll("-", "");
     }
 
